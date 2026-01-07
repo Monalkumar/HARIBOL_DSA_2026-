31cd1c7c6a70acb2b7b31bc0b4491d7858ac3f46
MonalKumar <monalshiv123@gmail.com>
2026-01-07 23:50:53 +0530

    flatten nested object with dot notation DSA

--- Arrays/array.js
+++ Arrays/array.js
@@ -1,2 +1,28 @@
-console.log("arrays dsa")
+// console.log("arrays dsa")
 
+const obj = {
+  a: 1,
+  b: {
+    c: 2,
+    d: {
+      e: 3
+    }
+  }
+};
+
+function flattenObject(obj, parentKey, result={}){
+    for (let key in obj){
+        const newKey = parentKey?`${parentKey}.${key}` : key;
+        if(typeof obj[key] === "object" && obj[key] !==null){
+         flattenObject(obj[key], newKey, result);
+        }
+        else{
+            result[newKey] = obj[key]
+        }
+        
+        
+    }
+    return result
+}
+const nums = flattenObject(obj);
+console.log(nums)
